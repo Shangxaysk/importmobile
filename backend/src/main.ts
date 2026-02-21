87b80585cad3f79b0e0fedbb88e625d1b7b15650
Shangxaysk <your_email@example.com>
2026-02-22 03:45:47 +0500

    fix: global replacement of all localhost endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { NestExpressApplication } from '@nestjs/platform-express'; // Qo'shildi
+import { join } from 'path'; // Qo'shildi
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // <NestExpressApplication> turini qo'shish shart
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  // 1. CORSNI YOQISH (Eng tepada bo'lsin)
+  // 1. CORSNI YOQISH
   app.enableCors({
-    origin: '*', // Hamma joydan ruxsat berish
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  // 2. RASMLARNI OCHIB BERISH (Static Assets)
+  // Bu qator brauzerga /uploads/ bilan boshlanadigan so'rovlarni 
+  // serverdagi 'uploads' papkasidan qidirishni aytadi
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
-  // 2. KEYIN SERVERNI ESHITISH
-  await app.listen(3000);
+  // 3. SERVERNI ESHITISH
+  await app.listen(process.env.PORT || 3000); // Render uchun portni dinamik qildik
 }
 bootstrap();
